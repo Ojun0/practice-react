13d484c82060cd7638cf5a118b869f973a8ec956
Ojun0 <101302803+Ojun0@users.noreply.github.com>
2022-10-13 17:22:42 +0900

    보조설명 추가

--- week3/functional.js
+++ week3/functional.js
@@ -8,7 +8,7 @@ const userList = [
         {name : 'hong', point : 100},
         {name : 'kim', point : 30},
         {name : 'Oh', point : 500},
-        {name : 'park', point : 80}
+        {name : 'park', point : 80} // 객페
 ]
 
 let newUsers = [];
@@ -23,10 +23,10 @@ console.log(userList, userList2)
 
 
 //Array.map(변환함수): 배열 -> 새 배열(변환된 원소) 원소의 개수만큼 반복
-const userList3 = userList.map(user => `<li>${user.name}, ${user.point}</li>`);
+const userList3 = userList.map(user => `<li>${user.name}, ${user.point}</li>`);// 배열을 받아서 HTML의 령태로 바꾼디 배열의 형태로 출력
 console.log(userList, userList3);
 
-const userList4 = userList.filter(user => user.point>=100).map(user => `<li>${user.name}, ${user.point} </li>`);
+const userList4 = userList.filter(user => user.point>=100/* 뱐환함수(원소) => 참 거짓 판단 */).map(user => `<li>${user.name}, ${user.point} </li>`); // 특정한 데이터만 추출할 때 filter 사용
 console.log(userList, userList4);
 
 //Array.reduce(변환함수, 초기값); 배열 -> 문자열, 객체, 배열 any 아무거나 다 변환 가능하다 .
@@ -35,7 +35,8 @@ const userList5 = userList.reduce((max_point_user, user) => user.point >= max_po
 console.log(userList, userList5);
 
 const userList6 = userList.reduce(
-    (obj, user) => {obj[user.name] = user.point; return obj},{}// 새로운 객체를 생성하기 위해 입력 입력안하면 객체가 계속 바뀜
+    (obj, user/** 초기값, 첫번쨰 원소 */) => {obj[user.name] = user.point; return obj},{}// 빈객체에 프로퍼티를 계속 넣음 새로운 객체를 생성하기 위해 입력 입력안하면 객체가 계속 바뀜
+    //이전 함수가 다음 함수에 영향을 끼치는 경우 사용, 객체를 하나더 만들 때
 );//최종 결과는 객체
 //입력 값도 배열 출력 값도 배열
 console.log(userList, userList6);
